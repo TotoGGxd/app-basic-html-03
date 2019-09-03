25b0962479869f8a1422f5a85d1e8a110fc5dcbe
jfcarocota <jfcarocota@gmail.com>
2019-09-03 13:44:51 -0700

    botones acomodados

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const drumButtons = document.querySelectorAll('button');
 
 drumButtons.forEach(
         button =>{
-            button.style.left = button.dataset.posx; 
-            button.style.top = button.dataset.posy; 
+            button.style.left = `${button.dataset.posx}px`; 
+            button.style.top = `${button.dataset.posy}px`;
         } 
 );
